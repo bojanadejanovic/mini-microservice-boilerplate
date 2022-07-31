ae46c4df2ae69ec46adc0f52c94979d95006a174
Bojana Borković <bojana.borkovic@outlook.com>
2022-07-31 13:24:22 +0200

    added deployments and services for posts and event bus service

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,18 +13,18 @@ app.post("/events", async (req, res) => {
   const event = req.body;
   events.push(event);
 
-  await axios.post("http://localhost:4000/events", event).catch((err) => {
+  await axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  await axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  await axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  await axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  // await axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // await axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // await axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
 
   res.send({ status: "OK" });
 });
@@ -34,5 +34,6 @@ app.get('/events', (req, res) => {
 });
 
 app.listen(4005, () => {
+  console.log('updated event bus');
   console.log('Listening on 4005...');
 });
